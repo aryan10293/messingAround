139e330cb0a3f837c0033d55e8d2b0397ddd28e1
aondrej ryan <aondrejryan11@gmail.com>
2025-10-31 01:40:27 -0700

    working on hashing passwords

---
+++ backend/config/verifyPassword.ts
@@ -0,0 +1,12 @@
+import argon2 from "argon2";
+// hashed equals the password from my database
+// plaintext equals the password the user provides
+const verifyPassword = async (hashedPassword:string, plainTextPassword:string) => {
+    try {
+        return  await argon2.verify(hashedPassword, plainTextPassword);;
+    } catch (err) {
+        console.log("wrong password");
+        // Handle error
+    }
+};
+export default verifyPassword;
